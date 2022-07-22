1c570365a554380eb0ab258fa04825b0c7261055
Rafiul29 <rafiulislam4472@gmail.com>
2022-07-22 22:05:49 +0600

    Meal entry

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import Layout from "../components/Layout";
 function MyApp({ Component, pageProps }) {
   return (
       <Layout>
-        return <Component {...pageProps} />
+         <Component {...pageProps} />
       </Layout>
       )
 }
