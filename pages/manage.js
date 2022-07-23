478776068d2bd23289933cffc2f4593ca300a131
Murad <muradahamed6262@gmail.com>
2022-07-23 11:29:10 +0600

    manage

--- pages/manage.js
+++ pages/manage.js
@@ -1,10 +1,18 @@
 import Layout from "../components/Layout";
-
+import Styles from '../styles/manage.module.css';
 const Manage=()=>{
 
     return(
         <Layout>
-        <h2> manage</h2>
+            <div className={Styles.dailymeal}>
+                <div className={Styles.dailyMealCalc}>
+                    Breakfast<p className='noOfMeal'>0</p>
+                </div>
+                <div  className={Styles.dailyMealCalc}>Lunch<p className='noOfMeal'>0</p>
+                </div>
+                <div  className={Styles.dailyMealCalc}>Dinner<p className='noOfMeal'>0</p>
+                </div>
+            </div>
         </Layout>
 
     )
