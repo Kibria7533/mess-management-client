115b0e9a098c06599e606edf1e96bc54105c703f
tenminuteversity <kibria7533>
2022-07-23 11:02:37 +0600

    bootstrap added

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
+// add bootstrap css
+import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/globals.css'
+import Head from "next/head";
 import Layout from "../components/Layout";
+
 function MyApp({ Component, pageProps }) {
   return (
-         <Component {...pageProps} />
+      <>
+          <Head>
+              <meta name="viewport" content="width=device-width, initial-scale=1" />
+          </Head>
+          <Component {...pageProps} />
+      </>
       )
 }
 
---
+++ pages/member-list.js
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+
+const MemberList=()=>{
+
+    return(
+        <Layout>
+            <h2> MemberList</h2>
+        </Layout>
+
+    )
+}
+export default  MemberList;
