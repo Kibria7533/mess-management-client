13dc309512173fa638a5f33a3d647e5583a4ab84
tenminuteversity <kibria7533>
2022-07-23 18:34:40 +0600

    cost page done

--- pages/cost.js
+++ pages/cost.js
@@ -1,12 +1,44 @@
 import Layout from "../components/Layout";
+import Link from "next/link";
+import { FaPlus ,FaTrashAlt} from 'react-icons/fa';
+import {Button} from "react-bootstrap";
 
-const Cost=()=>{
+const Cost = () => {
 
-    return(
+    return (
         <Layout>
-            <h2> Cost</h2>
+            <>
+
+                <div className="table-responsive">
+                    <table className="table">
+                        <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Date</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>19-07-2022</td>
+                            <td>kibria</td>
+                            <td>Pending</td>
+                            <td><FaTrashAlt/></td>
+                        </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+                <div className={'row d-flex justify-content-end'}>
+                    <Button className={''}><FaPlus/></Button>
+                </div>
+
+            </>
         </Layout>
 
     )
 }
-export default  Cost;
+export default Cost;
