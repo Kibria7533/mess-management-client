f3e3ca60949b8725ea9be16a6d03d5f5a9aee551
Rafiul29 <rafiulislam4472@gmail.com>
2022-07-26 18:10:31 +0600

    post and get api

--- pages/deposit.js
+++ pages/deposit.js
@@ -1,16 +1,51 @@
 import Layout from "../components/Layout";
 import {FaPlus, FaTrashAlt} from "react-icons/fa";
 import {Button} from "react-bootstrap";
-import {useState} from "react";
+import {useState,useEffect} from "react";
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
-
+import axios from "axios";
 
 const Deposit=()=>{
+    // Modal
     const [show, setShow] = useState(false);
-
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+    // deposit create usestate
+    const [date, setDate]=useState(" ");
+    const [name, setName]=useState(" ");
+    const [amount, setAmount]=useState(" ");
+    const [status, setStatus]=useState(" ");
+
+
+    //get deposit
+    const [depositList,setdepositlist]=useState(" ")
+    useEffect(()=>{
+        getDeposit();
+    })
+   const getDeposit=async()=>{
+    await axios.get("http://localhost:5000/deposit")
+        .then((data)=>{
+            console.log(data.data);
+            setdepositlist(data.data);
+        })
+        .catch((err)=>{
+            console.log(err)
+       })
+    }
+    const Save=async ()=>{
+        await axios.post("http://localhost:5000/deposit",{
+            date,
+            name,
+            amount,
+            status
+        },{headers: {'Accept': 'application/json',
+            'Content-Type': 'application/json'}})
+            .then((data)=>{
+                console.log(data);
+            })
+    }
 
     return(
         <Layout>
@@ -35,7 +70,21 @@ const Deposit=()=>{
                     </tr>
                     </thead>
                     <tbody>
+
+                    {/*{depositList.length>0 && depositList.map((depo,idx,index)=>{*/}
+                    {/*    return(*/}
+                    {/*        <tr key={idx}>*/}
+                    {/*            <td>{index}</td>*/}
+                    {/*            <td>{depo.createdAt}</td>*/}
+                    {/*            <td>{depo.name}</td>*/}
+                    {/*            <td>{depo.amount}</td>*/}
+                    {/*            <td>{depo.status}</td>*/}
+                    {/*        </tr>*/}
+                    {/*    )*/}
+                    {/*})}*/}
+
                     <tr>
+
                         <th scope="row">1</th>
                         <td>19-07-2022</td>
                         <td>kibria</td>
@@ -65,6 +114,9 @@ const Deposit=()=>{
                                 type="date"
                                 placeholder="Enter date"
                                 autoFocus
+                                onChange={(e)=>{
+                                    setDate(e.target.value)
+                                }}
                             />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
@@ -72,23 +124,40 @@ const Deposit=()=>{
                             <Form.Control
                                 type="text"
                                 placeholder="Enter your name"
+                                onChange={(e)=>{
+                                    setName(e.target.value);
+                                }}
                             />
                         </Form.Group>
+
                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                             <Form.Label>Amount</Form.Label>
                             <Form.Control
                                 type="number"
                                 placeholder="Enter your amount"
+                                onChange={(e)=>{
+                                    setAmount(e.target.value)
+                                }}
                             />
                         </Form.Group>
 
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label>Status</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter your amount"
+                                onChange={(e)=>{
+                                    setStatus(e.target.value)
+                                }}
+                            />
+                        </Form.Group>
                     </Form>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>
                         Close
                     </Button>
-                    <Button variant="primary" onClick={handleClose}>
+                    <Button variant="primary" onClick={()=>Save()}>
                         Add
                     </Button>
                 </Modal.Footer>
