20d5f23b7abc12e4fe881deaed5444a5a1a55ef2
golam.kibria@softbdltd.com <kibria7533@A>
2022-07-27 13:16:22 +0600

    member list add

--- pages/meal-report.js
+++ pages/meal-report.js
@@ -1,7 +1,47 @@
 import Layout from "../components/Layout";
 import Link from "next/link";
 import Style from '../styles/Table.module.css'
+import {useEffect, useState} from "react";
+import axios from "axios";
 const MealReport=()=>{
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    // deposit create usestate
+    const [date, setDate]=useState(" ");
+    const [name, setName]=useState(" ");
+    const [amount, setAmount]=useState(" ");
+    const [status, setStatus]=useState(" ");
+
+    //get deposit
+    const [mealReportList,setMealReportList]=useState([])
+
+    useEffect(()=>{
+        getMemberList();
+    })
+    const getMemberList=async()=>{
+        await axios.get("http://localhost:5000/meal-entry")
+            .then((data)=>{
+                console.log(data.data);
+                setMealReportList(data.data);
+            })
+            .catch((err)=>{
+                console.log(err)
+            })
+    }
+    const Save=async ()=>{
+        await axios.post("http://localhost:5000/member",{
+            date,
+            name,
+            amount,
+            status
+        },{headers: {'Accept': 'application/json',
+                'Content-Type': 'application/json'}})
+            .then((data)=>{
+                console.log(data);
+            })
+    }
 
     return(
         <Layout>
@@ -17,34 +57,17 @@ const MealReport=()=>{
                         </tr>
                         </thead>
                         <tbody>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>02-01-2022</td>
-                            <td className={`${Style.alignCntr}`}>0</td>
-                            <td className={`${Style.alignCntr}`}>0</td>
+                        {mealReportList.length>0 && mealReportList.map((member,idx)=>{
+                            return(
+                                <tr>
+                                    <th scope="row">2</th>
+                                    <td>02-01-2022</td>
+                                    <td className={`${Style.alignCntr}`}>0</td>
+                                    <td className={`${Style.alignCntr}`}>0</td>
 
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>03-01-2022</td>
-                            <td className={`${Style.alignCntr}`}>0</td>
-                            <td className={`${Style.alignCntr}`}>0</td>
-
-                        </tr>
-                        <tr>
-                            <th scope="row">4</th>
-                            <td>04-01-2022</td>
-                            <td className={`${Style.alignCntr}`}>0</td>
-                            <td className={`${Style.alignCntr}`}>0</td>
-
-                        </tr>
-                        <tr>
-                            <th scope="row">5</th>
-                            <td>05-01-2022</td>
-                            <td className={`${Style.alignCntr}`}>0</td>
-                            <td className={`${Style.alignCntr}`}>0</td>
-
-                        </tr>
+                                </tr>
+                            )
+                        })}
 
                         </tbody>
                     </table>
