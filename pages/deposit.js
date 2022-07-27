b071797b6add3a26513fe59308c7a65d10785997
Rafiul29 <rafiulislam4472@gmail.com>
2022-07-27 15:54:04 +0600

    meal entry

--- pages/deposit.js
+++ pages/deposit.js
@@ -44,14 +44,6 @@ const Deposit=()=>{
                 setShow(false);
             })
     }
-    const calendarStrings = {
-        lastDay : '[Yesterday at] LT',
-        sameDay : '[Today at] LT',
-        nextDay : '[Tomorrow at] LT',
-        lastWeek : '[last] dddd [at] LT',
-        nextWeek : 'dddd [at] LT',
-        sameElse : 'L'
-    };
 
     return(
         <Layout>
