ec4317b8c001de31d67c17b2e68c93ffd48fda2e
Rafiul29 <rafiulislam4472@gmail.com>
2022-07-29 12:28:55 +0600

    management

--- pages/management.js
+++ pages/management.js
@@ -1,10 +1,151 @@
 import Layout from "../components/Layout";
+import Card from "react-bootstrap/Card";
+import deposit from "./deposit";
+import stylemanagement from '../styles/management.module.css'
+import Link from "next/link";
+import { FaBeer } from 'react-icons/fa';
+import {AiOutlineArrowRight, AiOutlineDownload} from "react-icons/ai";
+import Button from "react-bootstrap/Button";
+import {MdFreeBreakfast, MdRestaurant} from "react-icons/md";
+
+
+
 
 const Management=()=>{
 
     return(
         <Layout>
-            <h2> Management</h2>
+            <div  className={`row ${stylemanagement.card}`}>
+
+                <div className={`col-md-4`}>
+                    <Card  style={{ width: '40rem', height:'25rem'}} >
+
+                        <Card.Body>
+                            <Card.Title className={stylemanagement.title}>Meal Of Time</Card.Title>
+                                <div className={stylemanagement.meal}>
+                                    <div className={stylemanagement.name}>
+                                        <span><MdFreeBreakfast/></span>
+                                        <h3>Breakfast</h3>
+                                    </div>
+                                    <span> Time </span>
+
+                                </div>
+
+                                <div className={stylemanagement.meal}>
+                                    <div className={stylemanagement.name}>
+                                        <span><MdFreeBreakfast/></span>
+                                        <h3>Lunch</h3>
+                                    </div>
+                                    <span> Time </span>
+
+                                </div>
+
+                                <div className={stylemanagement.meal}>
+                                    <div className={stylemanagement.name}>
+                                        <span><MdFreeBreakfast/></span>
+                                        <h3>Dinner</h3>
+                                    </div>
+                                    <span> Time </span>
+
+                                </div>
+
+                                <div className={stylemanagement.meal}>
+                                    <div className={stylemanagement.name} >
+                                        <span><MdFreeBreakfast/></span>
+                                        <h3>Day End</h3>
+                                    </div>
+                                    <span> Time </span>
+
+                                </div>
+
+                        </Card.Body>
+                    </Card>
+                </div>
+            </div>
+
+
+
+            <div  className={`row ${stylemanagement.card}`}>
+
+                <div className={`col-md-4`}>
+                    <Card  style={{ width: '40rem'}} >
+
+                        <Card.Body>
+                            <Link href={'/deposit'} >
+                          <div className={stylemanagement.cards} >
+
+                              <div className={stylemanagement.card1}>
+                                  <AiOutlineDownload />
+                                  <Card.Title>Add Deposit</Card.Title>
+                              </div>
+                              <AiOutlineArrowRight />
+                          </div>
+                            </Link>
+                        </Card.Body>
+                    </Card>
+                </div>
+            </div>
+
+            <div className={`row ${stylemanagement.card}`}>
+
+                <div className={`col-md-4`}>
+                    <Card  style={{ width: '40rem' }} >
+
+                        <Card.Body>
+                            <div className={stylemanagement.cards} >
+
+                                <div className={stylemanagement.card1}>
+                                    <AiOutlineDownload />
+                                    <Card.Title>Edit Deposit</Card.Title>
+                                </div>
+                                <AiOutlineArrowRight />
+                            </div>
+                        </Card.Body>
+                    </Card>
+                </div>
+            </div>
+
+            <div className={`row ${stylemanagement.card}`}>
+
+                <div className={`col-md-4`}>
+                    <Card  style={{ width: '40rem' }} >
+
+                        <Card.Body>
+
+                            <div className={stylemanagement.cards} >
+
+                                <div className={stylemanagement.card1}>
+                                    <MdRestaurant />
+                                    <Card.Title>Add Previous Meal</Card.Title>
+                                </div>
+                                <AiOutlineArrowRight />
+                            </div>
+
+
+                        </Card.Body>
+                    </Card>
+                </div>
+            </div>
+
+            <div className={`row ${stylemanagement.card}`}>
+
+                <div className={`col-md-4`}>
+                    <Card  style={{ width: '40rem' }} >
+
+                        <Card.Body>
+                            <div className={stylemanagement.cards} >
+
+                                <div className={stylemanagement.card1}>
+                                    <MdRestaurant />
+                                    <Card.Title>Edit Meal</Card.Title>
+                                </div>
+                                <AiOutlineArrowRight />
+                            </div>
+                        </Card.Body>
+                    </Card>
+                </div>
+            </div>
+
         </Layout>
 
     )
