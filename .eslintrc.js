80c1aec267507ce34d42d914f3d11be47846a20a
Rafiul29 <rafiulislam4472@gmail.com>
2022-09-03 19:36:40 +0600

    tsx convert

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+// module.exports = {
+// 	env: {
+// 		browser: true,
+// 		es2021: true,
+// 	},
+// 	extends: [
+// 		'plugin:react/recommended',
+// 		'xo',
+// 		'google',
+// 		'prettier'
+// 	],
+// 	overrides: [
+// 		{
+// 			extends: [
+// 				'xo-typescript',
+// 			],
+// 			files: [
+// 				'*.ts',
+// 				'*.tsx',
+// 			],
+// 		},
+// 	],
+// 	parserOptions: {
+// 		ecmaVersion: 'latest',
+// 		sourceType: 'module',
+// 	},
+// 	plugins: [
+// 		'react',
+// 	],
+// 	rules: {
+// 	},
+// 	settings: {
+// 		react: {
+// 		  version: "latest",
+// 		},
+// 	  },
+// };
