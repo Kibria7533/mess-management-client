43d3cfea0faa90cbbff913d22f368248dee8288f
Golam Kibria <golam.kibria@softbdltd.com>
2022-09-09 23:39:53 +0600

    fetch added

--- keycloak/keycloakConfigs.ts
+++ keycloak/keycloakConfigs.ts
@@ -1,16 +1,16 @@
 export const keycloakConfigs: any = {
   local: {
-    realm: 'future-nation-web',
-    clientId: 'future-nation',
+    realm: 'mess',
+    clientId: 'mess',
     clientSecret: 'aqXl7V7kXDhWjfoKDaQIQwFzmDMsFnnV',
     loginUrl:
-      'https://idp.futurenation.gov.bd/auth/realms/future-nation-web/protocol/openid-connect/auth',
+      'http://localhost:8088/auth/realms/future-nation-web/protocol/openid-connect/auth',
     logoutUrl:
-      'https://idp.futurenation.gov.bd/auth/realms/future-nation-web/protocol/openid-connect/logout',
+      'http://localhost:8088/auth/realms/future-nation-web/protocol/openid-connect/logout',
     passwordUpdateUrl:
-      'https://idp.futurenation.gov.bd/auth/realms/future-nation-web/protocol/openid-connect/auth',
+      'http://localhost:8088/auth/realms/future-nation-web/protocol/openid-connect/auth',
     tokenUrl:
-      'https://idp.futurenation.gov.bd/auth/realms/future-nation-web/protocol/openid-connect/token',
+      'http://localhost:8088/auth/realms/future-nation-web/protocol/openid-connect/token',
     loginCallbackUri: '/callback',
     logoutCallbackUri: '/logout',
     updatePasswordCallback: '/callback-update-password',
