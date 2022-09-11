2211bda71104d1a9a5da42dc42b835fc2af00141
Golam Kibria <golam.kibria@softbdltd.com>
2022-09-11 16:35:21 +0600

    elastic search removed

--- services/axiosInstance.js
+++ services/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const axiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_HOST || 'http://localhost:5000'
+    baseURL: process.env.NEXT_PUBLIC_HOST || 'http://ec2-13-57-246-76.us-west-1.compute.amazonaws.com:5000/bazar-list'
 });
 
 axiosInstance.interceptors.request.use((value) => {
